8e3f7cf1b04e0e6d3a76e5fd0d2cc97010178dad
leadweedy <dicorned@gmail.com>
2021-11-18 20:22:46 -0800

    full support for MJPEG and updated commandline interface

--- run-gst.sh
+++ run-gst.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
 # set internal IP of platform
-IP=192.168.0.230
+IP=192.168.1.102
 PORT=8080
 VIDEO=/dev/video0
 
 #desired resolution of video stream from camera
 #must be explicitly supported or pipeline fails
-WIDTH_IN=640
-HEIGHT_IN=480
+WIDTH_IN=800
+HEIGHT_IN=600
 FPS_IN=30
 
-#resolution to rescale to
-#any values are okay
-WIDTH_OUT=720
-HEIGHT_OUT=540
-FPS_OUT=30
 
-#choose an h264 encoder
-#vaapi = GPU hardware accelerated
-#x264 = cpu/software encoding
 
-# ENCODER=vaapih264enc
-ENCODER='x264enc tune=zerolatency qp-min=18 speed-preset=superfast'
 
-#payload has to match encoder
-PAYLOAD=rtph264pay
+## DEFAULTS
+
+# h264 encoder defaults to software encoding
+H264=x264enc
+
+# streaming format defaults to h264
+FORMAT=h264
 
 #format depends on what the camera outputs
 FORMAT=YUY2
@@ -33,18 +28,16 @@ FORMAT=YUY2
 #used to ingest a video source (e.g. webcam)
 SOURCE="v4l2src device"
 
+# extra arguments, unused
 EXTRA=""
 
 #explicity sets the capabilites of the source device,
 #helpful when it outputs multiple resolutions/formats
-CAPS="video/x-raw,width=$WIDTH_IN,height=$HEIGHT_IN,format=$FORMAT,framerate=$FPS_IN/1"
-
-#passed to videoscale module to resize the stream w/ bilinear filter
-RESCALE=" ! video/x-raw,width=$WIDTH_OUT,height=$HEIGHT_OUT,framerate=$FPS_OUT/1 "
+CAPS=",width=$WIDTH_IN,height=$HEIGHT_IN,format=$FORMAT,framerate=$FPS_IN/1"
 
 
 # adds input flags for more options
-while getopts :i:p:v:f option
+while getopts :i:p:v:F:e:f option
 do
     case "$option" in
     i)
@@ -61,36 +54,64 @@ do
          EXTRA="decodebin"
          CAPS=""
          ;;
+    F)
+         FORMAT=$OPTARG
+         ;;
+    e)
+         H264=$OPTARG
+         ;;
     *)
-        echo "Hmm, an invalid option was received. -p requires an argument."
-        echo "Here's the usage statement:"
+        echo "Usage:"
+        echo "  run-gst.sh [OPTION...]"
         echo ""
-        displayTpUsageStatement
-        return
+        echo "Help Options:"
+        echo "  -h                      Show help options"
+        echo ""
+        echo "Application Options:"
+        echo "  -i <#.#.#.#>            IP address of the recieving computer"
+        echo "  -p <#>                  Port number [default:8080]"
+        echo "  -v </dev/video#>        Video source, e.g. /dev/video0"
+        echo "  -f </file/path>         Enables streaming from a video file"
+        echo "  -F <h264 or MJPG>       Select stream format [default:h264]"
+        echo "  -e <CPU or GPU>         Chooses between vaapi (GPU) or software (CPU) encoding,"
+        echo "                          only valid for h264 [default:CPU]"
+        echo ""
+        exit
         ;;
         esac
 done
 
-
-
 #udp is used over tcp for latency. udp is allowed to drop packets. if tcp falls behind,
 #it tries to catch up and the stream lags behind
 
-#clockoverlay adds timestamp to corner of video
 
-# PIPELINE = raw video source -> scale/convert/overlays -> encode video -> pack into payload -> send to sink
+
+# PIPELINE = raw video source -> encode video -> pack into payload -> send to sink
+
+
+
+#vaapi = GPU hardware accelerated
+#x264 = cpu/software encoding
+ENCODER=vaapih264enc
+ENCODER='x264enc tune=zerolatency qp-min=18 speed-preset=superfast'
+
 
 
 # for h264 streams
-gst-launch-1.0 -v -e $SOURCE="$VIDEO" ! $EXTRA $CAPS ! \
-    videoscale $RESCALE ! videoconvert ! clockoverlay ! \
-    queue ! $ENCODER ! $PAYLOAD ! \
-    udpsink host=$IP port=$PORT
+if [ "$FORMAT" = 'h264' ]; then
+    gst-launch-1.0 -v -e $SOURCE=$VIDEO !
+        $EXTRA video/x-raw $CAPS ! \
+        queue ! $ENCODER ! rtph264pay ! \
+        udpsink host=$IP port=$PORT
 
 
-
-## MJPG stream (WIP)
-# gst-launch-1.0 -v -e v4l2src device=/dev/video$VIDEO num-buffers=800 do-timestamp=true ! \
-#     image/jpeg,width=$WIDTH, height=$HEIGHT, format=$FORMAT, framerate=$FPS/1 !\
-#     rtpjpegpay ! queue ! \
-#     udpsink host=$IP port=$PORT
+## MJPG stream
+elif [ "$FORMAT" = 'MJPG' ]; then
+    gst-launch-1.0 -v -e $SOURCE=$VIDEO ! \
+        $EXTRA image/jpeg $CAPS ! \
+        queue ! rtpjpegpay ! \
+        udpsink host=$IP port=$PORT
+        
+else
+    echo "Wrong stream format"
+fi
--- sink-gst.sh
+++ sink-gst.sh
@@ -1,36 +1,80 @@
 #!/bin/bash
 
 # set internal IP of platform
-IP=192.168.0.230
+IP=192.168.1.102
 PORT=8080
 
+#resolution to rescale to
+#any values are okay
+WIDTH_OUT=800
+HEIGHT_OUT=600
+
+
+
+## DEFAULTS
 
 CAPS="application/x-rtp,media=(string)video,encoding-name=(string)H264,clock-rate=(int)90000,payload=96"
 
+#passed to videoscale module to resize the stream w/ bilinear filter
+RESCALE=",width=$WIDTH_OUT,height=$HEIGHT_OUT"
 
-# adds input flag for port number
-while getopts :p: option
+FORMAT=h264
+
+#clockoverlay adds timestamp to corner of video
+# default = off
+CLOCK=
+
+
+
+
+
+
+# adds input flags for more options
+while getopts :p:F:c option
 do
     case "$option" in
+    i)
+         IP=$OPTARG
+         ;;
     p)
          PORT=$OPTARG
          ;;
+    F)
+         FORMAT=$OPTARG
+         ;;
+    c)
+         CLOCK="clockoverlay !"
+         ;;
     *)
-        echo "Hmm, an invalid option was received. -p requires an argument."
-        echo "Here's the usage statement:"
+        echo "Usage:"
+        echo "  sink-gst.sh [OPTION...]"
         echo ""
-        displayTpUsageStatement
-        return
+        echo "Help Options:"
+        echo "  -h                      Show help options"
+        echo ""
+        echo "Application Options:"
+        echo "  -i <#.#.#.#>            IP address of the recieving computer"
+        echo "  -p <#>                  Port number [default:8080]"
+        echo "  -F <h264 or MJPG>       Select stream format [default:h264]"
+        echo "  -c                      Enables clock overlay [default:off]"
+        echo ""
+        exit
         ;;
         esac
 done
 
 
+
+# PIPELINE = recieve video srouce -> decode video -> convert back into raw video -> rescale/overlays -> display
+
+
+
 ## video sink for any h264 encoder
-# GST_DEBUG="GST_TRACER:7" GST_TRACERS="latency(flags=pipeline+element+reported)" \
-gst-launch-1.0 -v -e udpsrc uri=udp://$IP:$PORT caps=$CAPS ! \
-rtph264depay ! queue ! avdec_h264 ! videoconvert ! \
-xvimagesink
+if [ "$FORMAT" = 'h264' ]; then
+    gst-launch-1.0 -v -e udpsrc uri=udp://$IP:$PORT caps=$CAPS ! \
+    rtph264depay ! queue ! avdec_h264 ! \
+    videoconvert ! videoscale ! video/x-raw $RESCALE ! $CLOCK \
+    autovideosink
 
 
 
@@ -47,5 +91,13 @@ xvimagesink
 
 
 ## MJPG
-# gst-launch-1.0 -v -e udpsrc uri=udp://$IP:$PORT ! \
-# application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! autovideosink
+elif [ "$FORMAT" = 'MJPG' ]; then
+    gst-launch-1.0 -v -e udpsrc uri=udp://$IP:$PORT ! \
+    application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! \
+    jpegdec ! videoconvert ! videoscale ! video/x-raw $RESCALE ! $CLOCK  \
+    autovideosink
+        
+else
+    echo "Wrong stream format"
+fi
+
